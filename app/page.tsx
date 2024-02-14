5d4e68463a0cc0463e5cc7d84b78248c3f919dba
akshaynmhc <104881846+akshaynmhc@users.noreply.github.com>
2024-02-14 16:04:42 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import {Input} from "@nextui-org/input";
+import {Button, ButtonGroup} from "@nextui-org/button";	
+
+export default function Home() {
+
+	const [videoId, setVideoId] = useState("");
+	const [thumbnails, setThumbnails] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const thumbnailUrl = "https://i.ytimg.com/vi/" + videoId + "/";
+		const thumbnailResolutions = [
+		"default",
+		"mqdefault",
+		"hqdefault",
+		"sddefault",
+		"maxresdefault",
+		];
+
+		const thumbnails = thumbnailResolutions.map((resolution) => ({
+		url: thumbnailUrl + resolution + ".jpg",
+		resolution: resolution,
+		}));
+
+		setThumbnails(thumbnails);
+	};
+
+	
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+		<div className="inline-block max-w-lg text-center justify-center">
+			<h1 className="text-4xl font-bold">YouTube Thumbnail</h1>
+			<h1 className="text-4xl font-bold text-violet-500">Downloader</h1>
+		</div>
+
+		<form
+			className="flex flex-row items-center justify-center gap-4 py-8 md:py-10 w-full"
+			onSubmit={handleSubmit}
+		>
+			<Input
+			type="text"
+			label="Enter YouTube Video ID"
+			value={videoId}
+			onChange={(e) => setVideoId(e.target.value)}
+			required
+			/>
+			<Button type="submit" color="primary">
+			Submit
+			</Button>
+		</form>
+
+		{thumbnails.length > 0 && (
+			<div className="flex flex-col items-center gap-4" id="thumbnails">
+			{thumbnails.map((thumbnail) => (
+				<img
+				key={thumbnail.resolution}
+				src={thumbnail.url}
+				className="thumbnail"
+				alt={`Thumbnail (${thumbnail.resolution})`}
+				style={{ cursor: "pointer" }}
+				/>
+			))}
+			</div>
+		)}
+		</section>
+	);
+}
---
+++ config/site.ts
@@ -0,0 +1,49 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "YouTube Video Thumbnail Downloader",
+	description: "Download YouTube Video Thumbnails with all possible resolutions.",
+	navItems: [
+		// {
+		// 	label: "Home",
+		// 	href: "/",
+		// },
+	],
+	navMenuItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		// {
+		// 	label: "Dashboard",
+		// 	href: "/dashboard",
+		// },
+		// {
+		// 	label: "Projects",
+		// 	href: "/projects",
+		// },
+		// {
+		// 	label: "Team",
+		// 	href: "/team",
+		// },
+		// {
+		// 	label: "Calendar",
+		// 	href: "/calendar",
+		// },
+		// {
+		// 	label: "Settings",
+		// 	href: "/settings",
+		// },
+		// {
+		// 	label: "Help & Feedback",
+		// 	href: "/help-feedback",
+		// },
+		// {
+		// 	label: "Logout",
+		// 	href: "/logout",
+		// },
+	],
+	links: {
+		website: "https://myheartcreative.com"
+	},
+};
