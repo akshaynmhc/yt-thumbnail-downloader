cd561ca2c001b3d9cd52e80575325c80bf6a514c
akshaynmhc <104881846+akshaynmhc@users.noreply.github.com>
2024-02-14 16:07:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import {Input} from "@nextui-org/input";
 import {Button, ButtonGroup} from "@nextui-org/button";	
 
